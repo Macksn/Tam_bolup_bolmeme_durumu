4b10b753809445404702ec63f25a50a41c60b6ca
Macksn <itsmacksn@yahoo.com>
2024-03-24 21:50:28 +0300

    Tam_bolup_bolmeme_durumu
    Class ile tam böler/bölmez

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CLASS_BOL_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bol eleman = new bol();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eleman.sayı = Convert.ToInt16(textBox1.Text);
+            eleman.bölen = Convert.ToInt16(textBox2.Text);
+            label3.Text = eleman.bul();
+
+
+        }
+    }
+}
---
+++ bol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CLASS_BOL_
+{
+    class bol
+    {
+        public int sayı , bölen ;
+        public string sonuc ;
+        public string bul()
+        {
+            if (sayı % bölen == 0)
+                sonuc = "tam böler";
+            else
+                sonuc = "tam bölmez";
+            return sonuc;
+        }
+
+    }
+}
